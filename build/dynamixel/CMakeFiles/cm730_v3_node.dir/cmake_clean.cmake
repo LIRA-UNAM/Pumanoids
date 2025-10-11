5ad2c719c30e7311e23671578654a260fa0c314e
MelMz-04 <mmatiasz.410gmail.com>
2025-10-10 20:04:54 -0600

    Migrate allpositions.cpp to ROS2

---
+++ build/dynamixel/CMakeFiles/cm730_v3_node.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cm730_v3_node.dir/src/cm730_utils.cpp.o"
+  "CMakeFiles/cm730_v3_node.dir/src/cm730_utils.cpp.o.d"
+  "CMakeFiles/cm730_v3_node.dir/src/cm730_v3.cpp.o"
+  "CMakeFiles/cm730_v3_node.dir/src/cm730_v3.cpp.o.d"
+  "CMakeFiles/cm730_v3_node.dir/src/servo_utils.cpp.o"
+  "CMakeFiles/cm730_v3_node.dir/src/servo_utils.cpp.o.d"
+  "cm730_v3_node"
+  "cm730_v3_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cm730_v3_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
