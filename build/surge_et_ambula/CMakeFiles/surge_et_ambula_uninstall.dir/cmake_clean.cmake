a2d2b58ebbfbee20bf162379ed102cb6af992b20
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-10-15 23:16:18 -0600

    cleanup build directories

--- build/surge_et_ambula/CMakeFiles/surge_et_ambula_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/surge_et_ambula_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/surge_et_ambula_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
