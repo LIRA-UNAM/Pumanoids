dac068d872566a394f97f14754395daee2340d0e
pumas_booster1 <lir.pumas@gmail.com>
2025-11-26 10:18:03 +0800

    Correct twist to T1 node (add stop if no new message received) add pantilt_to_t1

--- colcon_ws/src/hardware/booster/twist_to_t1/twist_to_t1/twist_to_t1.py
+++ colcon_ws/src/hardware/booster/twist_to_t1/twist_to_t1/twist_to_t1.py
@@ -5,17 +5,17 @@ from geometry_msgs.msg import Twist
 from booster_interface.srv import RpcService
 
 class TwistToT1Node(Node):
-    def get_rpc_request(self):
+    def get_rpc_request(self, vx, vy, vyaw):
         req = RpcService.Request()
         req.msg.api_id = 2001
-        req.msg.body = "{\"vx\": " + str(self.vx) + ", \"vy\": " + str(self.vy) + ", \"vyaw\": " + str(self.vyaw) + "}"
+        req.msg.body = "{\"vx\": " + str(vx) + ", \"vy\": " + str(vy) + ", \"vyaw\": " + str(vyaw) + "}"
         return req
     
     def callback_cmd_vel(self, msg):
         self.vx = msg.linear.x
         self.vy = msg.linear.y
         self.vyaw = msg.angular.z
-        print(msg)
+        self.new_cmd_data = True
         return 
     
     def __init__(self):
@@ -24,6 +24,7 @@ class TwistToT1Node(Node):
         self.vx = 0.0
         self.vy = 0.0
         self.vyaw = 0.0
+        self.new_cmd_data = False
         self.sub = self.create_subscription(Twist, '/cmd_vel', self.callback_cmd_vel, 10)
         self.clt_rpc = self.create_client(RpcService, '/booster_rpc_service')
 
@@ -33,20 +34,35 @@ class TwistToT1Node(Node):
         while not self.clt_rpc.wait_for_service(timeout_sec=1.0):
             print('Waiting for rpc service...')
         print("rpc service is now available...")
+
+        no_new_cmd_counter = 0;
         while rclpy.ok():
-            if abs(self.vx) >= 0.0 or abs(self.vy) >= 0.0 or abs(self.vyaw) >= 0.0:
-                req = self.get_rpc_request()
+            if self.new_cmd_data:
+                self.new_cmd_data = False
+                no_new_cmd_counter = 0
+                req = self.get_rpc_request(self.vx, self.vy, self.vyaw)
                 future = self.clt_rpc.call_async(req)
                 rclpy.spin_until_future_complete(self, future)
                 if future.result() is not None:
                     res = future.result()
-                    # print('Response: %s' % res.msg.body)
                 else:
                     self.get_logger().error('Service call failed %r' % (future.exception(),))
-            rclpy.spin_once(self)
-            time.sleep(0.5)
-
-
+            else:
+                no_new_cmd_counter += 1
+                if no_new_cmd_counter > 5:
+                    no_new_cmd_counter = 0
+                    req = self.get_rpc_request(0,0,0)
+                    future = self.clt_rpc.call_async(req)
+                    rclpy.spin_until_future_complete(self, future)
+                    if future.result() is not None:
+                        res = future.result()
+                    else:
+                        self.get_logger().error('Service call failed %r' % (future.exception(),))
+                        future = self.clt_rpc.call_async(req)
+                        rclpy.spin_until_future_complete(self, future)
+                    
+            rclpy.spin_once(self, timeout_sec=0)
+            time.sleep(0.2)
 
 def main(args=None):
     rclpy.init(args=args)
