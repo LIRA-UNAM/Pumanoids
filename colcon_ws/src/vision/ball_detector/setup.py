d53b793a153f0b0ccc17901e43530c20783ce8a9
pumas_booster1 <lir.pumas@gmail.com>
2025-11-26 10:20:37 +0800

    Modified ball_detector to pass model path by parameter

--- colcon_ws/src/vision/ball_detector/setup.py
+++ colcon_ws/src/vision/ball_detector/setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+
 
 package_name = 'ball_detector'
 
@@ -7,9 +10,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.pt'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
