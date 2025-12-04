9e896dfd43d7c9b1cbf21df01f7b29a6669b8273
ItzelGitHub <biddygonzalez@gmail.com>
2025-12-03 18:53:51 -0600

    se agrega MS para goalkeeper

--- colcon_ws/src/behaviors/goalkeeper/setup.py
+++ colcon_ws/src/behaviors/goalkeeper/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'goalkeeper_guard_executable = goalkeeper.goalkeeper_guard:main'
         ],
     },
 )
