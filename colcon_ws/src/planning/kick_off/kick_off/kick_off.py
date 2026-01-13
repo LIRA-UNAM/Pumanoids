4fff0e701470fb351b4e858684b056399c0aec2f
Miguel Garcia <garcia.miguel.onate@gmail.com>
2026-01-12 20:34:39 -0600

    Add kickoff node skeleton (minimal publisher)

---
+++ colcon_ws/src/planning/kick_off/kick_off/kick_off.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Bool
+
+
+class KickoffNode(Node):
+
+    def __init__(self):
+        super().__init__('kickoff_listener')
+        self.subscription = self.create_subscription(
+            Bool,
+            'kickoff/enable',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    kickoffnode = KickoffNode()
+
+    rclpy.spin(kickoffnode)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    kickoffnode.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
