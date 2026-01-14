46fbfd45507caf156affe8e8717bef54e77964ca
MiguelLZ12 <mglp1595@gmail.com>
2026-01-14 12:52:58 -0600

    Add T1_state_machine launch for Booster

---
+++ colcon_ws/src/surge_et_ambula/launch/T1_state_machine.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    start_position = LaunchConfiguration('start_position')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('start_position',default_value='center'),
+
+
+        Node(
+            package = 'game_planner',
+            executable = 'game_planner',
+            name = 'game_planner',
+            output = 'screen'
+
+        ),
+
+        Node(
+            package = 'twist_to_t1',
+            executable = 'twist_to_t1',
+            name = 'twist_to_t1',
+            output = 'screen',
+        ),
+
+        Node(
+            package = 'position_start',
+            executable = 'position_start',
+            name = 'position_start',
+            output = 'screen',
+            arguments = [start_position],
+        ),
+    ])
