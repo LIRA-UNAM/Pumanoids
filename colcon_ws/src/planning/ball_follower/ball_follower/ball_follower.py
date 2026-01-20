0f24c81b2850aefd2f8889ddf32ba4000bcce05c
pumas_booster1 <lir.pumas@gmail.com>
2026-01-21 07:22:50 +0800

    Changes on ball_detector

--- colcon_ws/src/planning/ball_follower/ball_follower/ball_follower.py
+++ colcon_ws/src/planning/ball_follower/ball_follower/ball_follower.py
@@ -3,6 +3,7 @@ import math
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Bool
 from pumas_vision_msgs.msg import VisionObject
 
 center_x = 640
@@ -14,6 +15,9 @@ class BallFollowerNode(Node):
         self.current_head_tilt = msg.position[1]
         
     def callback_ball(self, msg):
+        if not self.is_enabled:
+            return
+
         ball_center_x = msg.x
         ball_center_y = msg.y
         #print(f"Ball detected at x: {ball_center_x}, y: {ball_center_y}")
@@ -33,12 +37,30 @@ class BallFollowerNode(Node):
     def __init__(self):
         print("INITIALIZING BALL FOLLOWER NODE - ")
         super().__init__("ball_follower")
+        self.is_enabled = False
         self.current_head_pan  = 0
         self.current_head_tilt = 0
         self.sub_ball = self.create_subscription(VisionObject, '/vision/ball', self.callback_ball, 1)
         self.pub_cmd_vel = self.create_publisher(Twist, '/cmd_vel', 1)
         self.sub_joints  = self.create_subscription(JointState, "/joint_states", self.callback_joint_states, 1)
+        self.sub_enable = self.create_subscription(Bool, "/ball_follower/enable", self.callback_enable, 1)
+        
+        self.get_logger().info("Waiting for enable signal")
     
+    def callback_enable(self, msg):
+        if msg.data:
+            if not self.is_enabled:
+                self.get_logger().info("Ball Follower Enabled")
+            self.is_enabled = True
+        else:
+            if self.is_enabled:
+                self.get_logger().info("Ball Follower Disabled")
+                cmd_vel_msg = Twist()
+                cmd_vel_msg.linear.x = 0.0
+                cmd_vel_msg.angular.z = 0.0
+                self.pub_cmd_vel.publish(cmd_vel_msg)
+            self.is_enabled = False
+        
 
 def main(args=None):
     rclpy.init(args=args)
