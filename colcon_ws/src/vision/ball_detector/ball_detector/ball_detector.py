3bbb4b94b2a61e241911dd0d87895af767080802
gmsebastian <sgmartes19@comunidad.unam.mx>
2026-03-05 22:53:45 -0600

    hid ball_detector preview window

--- colcon_ws/src/vision/ball_detector/ball_detector/ball_detector.py
+++ colcon_ws/src/vision/ball_detector/ball_detector/ball_detector.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
+from rcl_interfaces.msg import SetParametersResult
 from ament_index_python.packages import get_package_share_directory
 import cv2
 from sensor_msgs.msg import Image, JointState
@@ -49,6 +51,9 @@ class BallDetectorNode(Node):
         bboxes = results[0].boxes.xywh.cpu().tolist()
         
         detected_goalposts = []
+        goal_center_found = False
+        avg_img_x = 0.0
+        avg_img_y = 0.0
         
         for i in range(len(idxs)):
             name=results[0].names[idxs[i]]
@@ -74,37 +79,43 @@ class BallDetectorNode(Node):
                     'width': width,
                     'height': height
                 })
-                
-        annotated_frame = results[0].plot()
-        
+
+
         if len(detected_goalposts) >= 2:
-                detected_goalposts.sort(key=lambda p: p['confidence'], reverse=True)
-                p1 = detected_goalposts[0]
-                p2 = detected_goalposts[1]
-                    
-                goal_center_x = (p1['x'] + p2['x']) / 2.0
-                goal_center_y = (p1['y'] + p2['y']) / 2.0
-                    
-                avg_img_x = (p1['img_x'] + p2['img_x']) / 2.0
-                avg_img_y = (p1['img_y'] + p2['img_y']) / 2.0
-                avg_width = (p1['width'] + p2['width']) / 2.0
-                avg_height = (p1['height'] + p2['height']) / 2.0
-                avg_confidence = (p1['confidence'] + p2['confidence']) / 2.0
+            detected_goalposts.sort(key=lambda p: p['confidence'], reverse=True)
+            p1 = detected_goalposts[0]
+            p2 = detected_goalposts[1]
                 
-                vision_obj_msg = self.get_vision_object_msg("goal_center", float(avg_confidence), avg_img_x, avg_img_y, avg_width, avg_height, goal_center_x, goal_center_y)
-                self.pub_goal.publish(vision_obj_msg)
+            goal_center_x = (p1['x'] + p2['x']) / 2.0
+            goal_center_y = (p1['y'] + p2['y']) / 2.0
                 
+            avg_img_x = (p1['img_x'] + p2['img_x']) / 2.0
+            avg_img_y = (p1['img_y'] + p2['img_y']) / 2.0
+            goal_center_found = True
+            avg_width = (p1['width'] + p2['width']) / 2.0
+            avg_height = (p1['height'] + p2['height']) / 2.0
+            avg_confidence = (p1['confidence'] + p2['confidence']) / 2.0
+            
+            vision_obj_msg = self.get_vision_object_msg("goal_center", float(avg_confidence), avg_img_x, avg_img_y, avg_width, avg_height, goal_center_x, goal_center_y)
+            self.pub_goal.publish(vision_obj_msg)
+            
+        elif len(detected_goalposts) == 1:
+            p = detected_goalposts[0]
+            vision_obj_msg = self.get_vision_object_msg("goalpost", float(p['confidence']), p['img_x'], p['img_y'], p['width'], p['height'], p['x'], p['y'])
+            self.pub_goal.publish(vision_obj_msg)
+
+        if self.get_parameter('show_debug_window').get_parameter_value().bool_value:
+            annotated_frame = results[0].plot()
+            if goal_center_found:
                 cv2.circle(annotated_frame, (int(avg_img_x), int(avg_img_y)), 10, (0, 255, 0), -1)
                 cv2.putText(annotated_frame, "GOAL CENTER", (int(avg_img_x) - 40, int(avg_img_y) - 15), 
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-        elif len(detected_goalposts) == 1:
-                p = detected_goalposts[0]
-                vision_obj_msg = self.get_vision_object_msg("goalpost", float(p['confidence']), p['img_x'], p['img_y'], p['width'], p['height'], p['x'], p['y'])
-                self.pub_goal.publish(vision_obj_msg)
+            cv2.imshow("YOLO Detection", annotated_frame)
+            cv2.waitKey(1)
+        else:
+            pass
         
 
-        cv2.imshow("YOLO Detection", annotated_frame)
-        cv2.waitKey(1)
     
     def get_ball_position(self, img_x, img_y, img_width, img_height):
         ball_x = 0.0
@@ -143,6 +154,8 @@ class BallDetectorNode(Node):
         self.br = CvBridge()
         model_path = os.path.join(get_package_share_directory("ball_detector"), "models", "yolov8_center.pt")
         self.declare_parameter('model_path', model_path)
+        self.declare_parameter('show_debug_window', False)
+        self.show_debug = self.get_parameter('show_debug_window').get_parameter_value().bool_value
         model_path  = self.get_parameter('model_path').get_parameter_value().string_value
         self.sub_img = self.create_subscription(Image, '/camera/color/image_raw', self.callback_img, 1)
         self.sub_joints = self.create_subscription(JointState, '/joint_states', self.callback_joint_states, 1)
