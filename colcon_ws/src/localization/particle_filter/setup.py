a0d2deddec1e93f7c46d7b0241dfac45b48d8ce2
Ruth Moreno <ruthmced@gmail.com>
2026-03-17 13:29:35 -0600

    localization added, not tested

---
+++ colcon_ws/src/localization/particle_filter/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'particle_filter'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ruthmced',
+    maintainer_email='ruthmced@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'map_node = particle_filter.map:main',
+        'mcl_node=particle_filter.mcl_node:main',
+        'detector = landmarks_detection.detector_node:main',
+        ],
+    },
+)
