4e796edb0310614a73f257ab797b2c34388c73a6
EdgarSilva77 <ing_edd@outlook.com>
2026-03-27 21:48:30 -0600

    Primera version del face detector con mediapipe

---
+++ colcon_ws/src/vision/face_detector/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'face_detector'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='edd',
+    maintainer_email='edd@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'face_detector = face_detector.face_detector:main',
+        ],
+    },
+)
