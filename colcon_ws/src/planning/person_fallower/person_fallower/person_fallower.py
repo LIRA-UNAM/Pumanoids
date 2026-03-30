83368914167328c98e07aec9374cc6d9c89bed11
Dps8 <darienps8r@gmail.com>
2026-03-30 13:32:23 -0600

    feat: Update target face height and image resolution for improved detection accuracy

--- colcon_ws/src/planning/person_fallower/person_fallower/person_fallower.py
+++ colcon_ws/src/planning/person_fallower/person_fallower/person_fallower.py
@@ -26,7 +26,7 @@ class PersonFollowerBase(Node):
         # Variables de estado interno
         self.head_pan_angle = 0.0
         self.distance_x = 0.5
-        self.face_x_img = 80 # Centro predeterminado para 160x120
+        self.face_x_img = 320 # Centro predeterminado para 640x480
         self.enabled = False  # Por defecto apagado, la máquina de estados lo encenderá
         
         # Suscriptores
@@ -72,8 +72,8 @@ class PersonFollowerBase(Node):
         twist = Twist()
         
         # --- Lógica idéntica a ball_follower.py ---
-        # Calculamos el error en imagen (imagen de 160px, centro en 80)
-        error_img = (-self.face_x_img + 80) / 160.0
+        # Calculamos el error en imagen (imagen de 640px, centro en 320)
+        error_img = (-self.face_x_img + 320) / 640.0
         if error_img < 0:
             error_img = -math.sqrt(-error_img)
         else:
@@ -97,7 +97,7 @@ class PersonFollowerBase(Node):
         self.pub_cmd_vel.publish(twist)
         
         self.distance_x = (self.distance_x * 0.8) + (0.5 * 0.2) # Decaimiento suave
-        self.face_x_img = (self.face_x_img * 0.8) + (80.0 * 0.2) # Decaimiento suave al centro
+        self.face_x_img = (self.face_x_img * 0.8) + (320.0 * 0.2) # Decaimiento suave al centro
 
 def main(args=None):
     rclpy.init(args=args)
