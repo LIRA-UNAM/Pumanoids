59a4d42e6e088d1a305cf644cd2419001cee67e6
FriasCamille <camfriher@comunidad.unam.mx>
2026-06-05 18:28:05 -0600

    Merge of t1 twist package

---
+++ colcon_ws/src/hardware/booster/new_twist_to_t1/launch/t1_twist.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='new_twist_to_t1',
+            executable='twist_to_t1',
+            name='twist_to_t1',
+            output='screen'),
+        Node(
+            package='new_twist_to_t1',
+            executable='pantilt_to_t1',
+            name='pantilt_to_t1',
+            output='screen'),
+        Node(
+            package='new_twist_to_t1',
+            executable='odom_to_tf',
+            name='odom_to_tf',
+            output='screen')
+    ])
