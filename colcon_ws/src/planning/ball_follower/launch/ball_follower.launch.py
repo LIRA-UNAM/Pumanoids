6f8aec0feffbfd2432ea3d1693f198a08361715b
FriasCamille <camfriher@comunidad.unam.mx>
2026-06-08 21:54:51 -0600

    Added read yaml logic for ball follower launches

--- colcon_ws/src/planning/ball_follower/launch/ball_follower.launch.py
+++ colcon_ws/src/planning/ball_follower/launch/ball_follower.launch.py
@@ -10,6 +10,9 @@ import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument 
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+import yaml
 
 def generate_launch_description():
     model_path = os.path.join(
@@ -17,16 +20,31 @@ def generate_launch_description():
         'models',
         'yolov8_center_sys_low.engine' # <-- Change this to the model you want to use
     )
+    yaml_path = str( PathJoinSubstitution([
+        get_package_share_directory('config_files'),
+        'robot',
+        PythonExpression([
+            LaunchConfiguration('robot'),
+            " + '.yaml'"
+        ])
+    ]))
+    with open(yaml_path, "r") as file:
+        params = yaml.safe_load(file)
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'robot',
+            default_value='t1',
+            description = 'Modelo del robot'
+        ),
         # Vision node to detect field elements
         Node(
             package='new_ball_detector',
             executable='ball_detector',
             name='ball_detector',
             parameters=[{
-                'hfov': 93.0,
-                'vfov': 101.0,
-                'head_z': 0.85,
+                'hfov': params['camera']['hfov_deg'],
+                'vfov': params['camera']['vfov_deg'],
+                'head_z': params['head']['height_m'],
                 'ball_radius': 0.07, # NOTE: RoboCup ball is 0.11 !!!!
                 'show_debug': True, # <-- Only set to True if testing through ethernet
                 'model_path': model_path
