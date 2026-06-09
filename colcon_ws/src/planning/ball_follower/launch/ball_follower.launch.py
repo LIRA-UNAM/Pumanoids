87ca6e1ba53940742110afc8c335c4016c6715f4
Booster <booster@boosterobotics.com>
2026-06-10 04:27:44 +0800

    Working conditional ball_follower launch file

--- colcon_ws/src/planning/ball_follower/launch/ball_follower.launch.py
+++ colcon_ws/src/planning/ball_follower/launch/ball_follower.launch.py
@@ -2,41 +2,43 @@
 # This is the main launch file to use the ball_follower nodes.
 # ---------------------------
 # Other launch files that use or want to use the ball_follower nodes,
-# like the master launch, summon this.
+# like the master launch, must summon this file.
 
-# NOTE: For testing, there are other launch files inside this package.
+# NOTE: For testing with robot movement, there are other launch files inside this package.
 
 import os
-from ament_index_python import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument 
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
 import yaml
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription, LaunchContext
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 
-def generate_launch_description():
+def launch_setup(context: LaunchContext, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot').perform(context)
+    
+    yaml_path = os.path.join(
+        get_package_share_directory('config_files'),
+        'robots',
+        f'{robot_name}.yaml'
+    )
+    
+    with open(yaml_path, "r") as file:
+        params = yaml.safe_load(file)
+        
     model_path = os.path.join(
         get_package_share_directory('new_ball_detector'),
         'models',
-        'yolov8_center_sys_low.engine' # <-- Change this to the model you want to use
+        'yolov8_center_sys_low.engine'
     )
-    yaml_path = str( PathJoinSubstitution([
-        get_package_share_directory('config_files'),
-        'robot',
-        PythonExpression([
-            LaunchConfiguration('robot'),
-            " + '.yaml'"
-        ])
-    ]))
-    with open(yaml_path, "r") as file:
-        params = yaml.safe_load(file)
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'robot',
-            default_value='t1',
-            description = 'Modelo del robot'
+
+    nodes = [
+        Node(
+            package = 'joint_states_package',
+            executable = 'joints_service',
+            name = 'joints_service',
+            output = 'screen'
         ),
-        # Vision node to detect field elements
         Node(
             package='new_ball_detector',
             executable='ball_detector',
@@ -46,20 +48,43 @@ def generate_launch_description():
                 'vfov': params['camera']['vfov_deg'],
                 'head_z': params['head']['height_m'],
                 'ball_radius': 0.07, # NOTE: RoboCup ball is 0.11 !!!!
-                'show_debug': True, # <-- Only set to True if testing through ethernet
+                'show_debug': True,
                 'model_path': model_path
             }],
-            output='screen'),
-        # To follow the ball with the head
+            output='screen'
+        ),
         Node(
             package='ball_follower',
             executable='head_ball_follower',
             name='head_ball_follower',
-            output='screen'),
-        # To follow the ball walking
+            output='screen'
+        ),
         Node(
             package='ball_follower',
             executable='ball_follower',
             name='ball_follower',
-            output='screen')
+            output='screen'
+        )
+    ]
+    if robot_name == 'k1':
+        nodes.append(
+            Node(
+                package='boosterk1_image_proc',
+                executable='nv12_converter_node',
+                name='nv12_converter_node',
+                output='screen',
+            )
+        )
+
+    return nodes
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'robot',
+            default_value='t1',
+            description='Modelo del robot'
+        ),
+        OpaqueFunction(function=launch_setup)
     ])
