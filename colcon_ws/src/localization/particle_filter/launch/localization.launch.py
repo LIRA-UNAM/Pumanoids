b68dc15cc60b45f5a22625515e6927a6685fd23b
Booster <booster@boosterobotics.com>
2026-06-11 10:52:08 +0800

    tests in new branch

--- colcon_ws/src/localization/particle_filter/launch/localization.launch.py
+++ colcon_ws/src/localization/particle_filter/launch/localization.launch.py
@@ -1,7 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+import os
+from ament_index_python import get_package_share_directory
 def generate_launch_description():
+    model_path = os.path.join(
+        get_package_share_directory('new_ball_detector'),
+        'models',
+        'yolov8_center_sys_low.engine' # <-- Change this to the model you want to use
+    )
     return LaunchDescription([
         
            
@@ -20,15 +26,19 @@ def generate_launch_description():
         ),
        
      Node(
-            package = 'particle_filter',
-            executable = 'detector',
-            name = 'detector',
-            output = 'screen',
+            package='new_ball_detector',
+            executable='ball_detector',
+            name='ball_detector',
+            parameters=[{
+                'show_debug': True,                
+                'model_path': model_path
+                    }],
+            output='screen'
         ),
        
      Node(
             package = 'particle_filter',
-            executable = 'mirror_map_node',
+            executable = 'map_node',
             name = 'map_node',
             output = 'screen',
         ),
@@ -39,6 +49,12 @@ def generate_launch_description():
             name = 'mcl_node',
             output = 'screen',
         ),
+     Node(
+        package = 'joint_states_package',
+        executable = 'joints_service',
+        name = 'joints_service',
+    ),
+
      Node(
         package = 'new_twist_to_k1',
         executable = 'odom_to_tf',
