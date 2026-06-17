7ea254f5b135d044eefd8bb1fca8db29a50411a4
biddyitzel.gonzalez <biddyitzel.gonzalez@bbva.com>
2026-06-16 18:05:52 -0600

    se agrega señal habilitadora a SM Goalkeeper

--- colcon_ws/src/behaviors/goalkeeper/launch/main.launch.py
+++ colcon_ws/src/behaviors/goalkeeper/launch/main.launch.py
@@ -3,6 +3,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -13,11 +14,19 @@ def generate_launch_description():
             os.path.join(
                 get_package_share_directory('my_robot'),
                 'launch',
-                'ball_follower.launch.py'
+                'test_with_movement.launch.py'
             )
         )
     )
 
+    goalkeeper_node = Node(
+        package='goalkeeper',
+        executable='goalkeeper_guard_executable',
+        name='goalkeeper_guard',
+        output='screen'
+    )
+
     return LaunchDescription([
-        ball_follower_launch
+        ball_follower_launch,
+        goalkeeper_node
     ])
